16053408ab20d5e148d3cc1cb17282026bb6831f
kubiklex <LexSoto@Alejandros-MacBook-Pro.local>
2017-03-05 13:45:46 -0600

    first

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link href="css/reset.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/movil.css" rel="stylesheet">
+	<title>PROMA</title>
+</head>
+<body>
+	<header>
+		<div class="top-black">
+			<div class="wrapp perfiles"><a href="#">Contructor</a> | <a href="#">Particular</a></div>
+		</div>
+		<div class="wrapp">
+			<div id="logo"><a href="#"></a></div>
+		</div>
+	</header>
+	<div class="bc-ground">
+	</div>
+
+	<section>
+		<div class="wrapp first-table">
+			<div class="fluid">
+				<div class="cruz"></div>
+			</div>
+			<div class="table second-table">
+				<div class="hlf txt-white">
+					<h1>EN PROMA<br> NACIMOS <br>PARA DARTE<br> MÁS.</h1>
+				</div>
+				<div class="hlf sala"></div>
+			</div>
+		</div>
+	</section>
+	<div class="clear"></div>
+	<section>
+		<div class="hlf purple">
+			<div class="wrapp txt-constr">
+				<h3 class="txt-orange">CONSTRUYE<br>+ FÁCIL</h3>
+				<p>
+					Somos especialistas en acabados y tu mejor aliado en proyectos de construcción, remodelación y decoración.
+				</p>
+				<p>
+					Queremos dar vida a tus proyectos con la mejor atención, precios competitivos y un amplio catálogo que integra las mejores marcas de construcción y acabados.</p>
+				<p>
+					En PROMA nacimos para darte más.
+				</p>
+				<p>
+					Más opciones para construir más fácil. Más ahorro que te permita realizar más proyectos. Más ideas para decorar tus espacios con más estilo.
+				</p>
+			</div>
+		</div>
+		<div class="hlf">
+			<div class="img-sala"></div>
+		</div>
+	</section>
+	<div class="clear"></div>
+	<section>
+		<div class="bck-sala"></div>
+	</section>
+	<div class="clear"></div>
+	<section>
+		<div class="wrapp third-cruz">
+			<div class="fluid">
+				<div class="cruz"></div>
+			</div>
+		</div>
+		<div class="bck-idea table">
+			<div class="wrapp verti ideas">
+				<h3>+IDEAS</h3>
+				<h4>INSPIRACIÓN PARA TUS PROYECTOS</h4>
+				<button class="btn-purple">CONOCE NUESTRA GALERÍA</button>
+			</div>
+		</div>
+	</section>
+	<div class="clear"></div>
+	<section>
+		<div class="wrapp beneficios table">
+			<h3 class="">+BENEFICIOS</h3>
+			<div class="clear"></div>
+			<div class="hlf izqu">
+				<div>
+					<i class="fa fa-handshake-o" aria-hidden="true"></i>TU MEJOR ALIADO
+					<p>
+						En <strong>PROMA</strong> somos mucho más que un
+						catálogo de productos; <strong>somos parte de tu proyecto.</strong>
+					</p>
+				</div>
+				<div>
+					<i class="fa fa-truck" aria-hidden="true"></i>ENVIO
+					<p>
+						Llevamos el material hasta el sitio que nos indiques en tiempo y forma.
+					</p>
+				</div>
+				<div>
+					<i class="fa fa-star" aria-hidden="true"></i>ACABADOS CON ESTILO
+					<p>
+						Contamos con una gran gama de materiales para explotar la belleza de tu espacio.
+					</p>
+				</div>
+			</div>
+			<div class="hlf dere">
+				<div>
+					<i class="fa fa-sun-o" aria-hidden="true"></i>CALENTADORES SOLARES
+					<p>
+						Ofrecemos alternativas amigables con el medio ambiente y tu bolsillo.
+					</p>
+				</div>
+				<div>
+					<i class="fa fa-user-circle-o" aria-hidden="true"></i>ASESORÍA ESPECIALIZADA
+					<p>
+						Contamos con un equipo de especialistas en 
+						rendimiento, uso y aplicación de cada material.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="clear"></div>
+	<section>
+		<div class="hlf designer">
+		</div>
+		<div class="hlf purple experience">
+			<div class="wrapp">
+				<h3>+EXPERIENCIA</h3>
+				<h4>LA EXPERIENCIA SE GANA CON RESULTADOS</h4>
+				<ul class="satisfaction">
+					<li><span class="numeralia">357</span> CLIENTES FELICES</li>
+					<li><span class="numeralia">250</span> PROYECTOS EXITOSOS</li>
+					<li><span class="numeralia">3589</span> PEDIDOS ENTREGADOS</li>
+					<li><span class="numeralia">1460</span> DÍAS CONSTRUYENDO</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<div class="clear"></div>
+	<section>
+		<div class="wrapp opciones table">
+			<h3 class="">+OPCIONES</h3>
+			<h4>CATÁLOGO DE PRODUCTOS</h4>
+			<div class="clear"></div>
+			<p>Las mejores marcas de México.</p>
+			<p>Encuentra todo lo que necesitas para tu proyecto de</p>
+			<p>contrucción, remodelación o decoración en un solo sitio.</p>
+		</div>
+	</section>
+	<div class="clear"></div>
+	<section>
+		<div class="wrapp fourth-cruz">
+			<div class="fluid">
+				<div class="cruz"></div>
+			</div>
+		</div>
+		<div style="min-height: 200px; background: red">
+			<div class="grid4" style="min-height: 200px;">.</div>
+			<div class="grid4" style="min-height: 200px;">.</div>
+			<div class="grid4" style="min-height: 200px;">.</div>
+			<div class="grid4" style="min-height: 200px;">.</div>
+			<div class="grid4" style="min-height: 200px;">.</div>
+			<div class="grid4" style="min-height: 200px;">.</div>
+			<div class="grid4" style="min-height: 200px;">.</div>
+		</div>
+	</section>
+	<div class="clear"></div>
+	<section>
+		<div class="wrapp opciones table">
+			<h3 class="">+BENEFICIOS</h3>
+			<h4>CONTACTO</h4>
+			<div class="clear"></div>
+			<div class="form">
+				<form action="" method="post">
+					<div class="hlf">
+						<input type="text" name="" placeholder="NOMBRE">
+					</div>
+					<div class="hlf">
+						<input type="text" name="" placeholder="EMAIL">
+					</div>
+					<div class="fluid">
+					<textarea placeholder="MENSAJE"></textarea>						
+					</div>
+					<input type="submit" name="ENVIAR MENSAJE" class="btn-purple">
+				</form>
+			</div>
+		</div>
+	</section>
+	<div class="clear"></div>
+	<footer>
+		<div class="wrapp">
+			<div class="logo"><a href="#"></a></div>
+		</div>
+	</footer>
+</body>
+</html>
